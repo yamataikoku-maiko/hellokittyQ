47ab82737adfcb6a7a7e9df98d92ecaee3b8d3b3
yamataikoku-maiko <78639158+yamataikoku-maiko@users.noreply.github.com>
2021-02-06 18:03:57 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('#form').submit(function() {
+        var selectValue = $('#select-form').val();
+        var textValue = $('#text-form').val();
+
+        if(textValue==''){
+            $('#error-message').text('理由を記入してください');
+        }else{
+          $('#error-message').text('');
+        }
+    
+        $('#output-select').text(selectValue);
+        $('#output-text').text(textValue);
+        return false;
+  });
+});
